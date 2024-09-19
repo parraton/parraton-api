607564a9aa3de4faa08d49fe5372a67bb4edb066
KStasi <nastasiakondaurova@gmail.com>
2024-09-20 00:54:48 +0300

    return @orbs-network

--- src/ton-client.ts
+++ src/ton-client.ts
@@ -1,12 +1,11 @@
 import { TonClient4 } from '@ton/ton';
-// import { getHttpV4Endpoint } from '@orbs-network/ton-access';
 import { TON_CLIENT_URL } from './config';
+import { getHttpV4Endpoint } from '@orbs-network/ton-access';
 
-// const endpointPromise = getHttpV4Endpoint();
+const endpointPromise = getHttpV4Endpoint();
 
 export const tonClient = (async () => {
-  const endpoint = TON_CLIENT_URL;
-  // const endpoint = TON_CLIENT_URL || (await endpointPromise);
+  const endpoint = TON_CLIENT_URL || (await endpointPromise);
 
   return new TonClient4({
     endpoint,
