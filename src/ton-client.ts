0794ce8eebd38bfc61d00157caab84a281a9ff6a
KStasi <nastasiakondaurova@gmail.com>
2024-09-20 00:48:02 +0300

    try temporary disable orbs-network

--- src/ton-client.ts
+++ src/ton-client.ts
@@ -1,11 +1,12 @@
 import { TonClient4 } from '@ton/ton';
-import { getHttpV4Endpoint } from '@orbs-network/ton-access';
+// import { getHttpV4Endpoint } from '@orbs-network/ton-access';
 import { TON_CLIENT_URL } from './config';
 
-const endpointPromise = getHttpV4Endpoint();
+// const endpointPromise = getHttpV4Endpoint();
 
 export const tonClient = (async () => {
-  const endpoint = TON_CLIENT_URL || (await endpointPromise);
+  const endpoint = TON_CLIENT_URL;
+  // const endpoint = TON_CLIENT_URL || (await endpointPromise);
 
   return new TonClient4({
     endpoint,
