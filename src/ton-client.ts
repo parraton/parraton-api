5eaf011c792a894fbdf4f84d830ca94326d068ca
KStasi <nastasiakondaurova@gmail.com>
2024-09-20 16:03:20 +0300

    use @orbs-network for ton-client

--- src/ton-client.ts
+++ src/ton-client.ts
@@ -1,11 +1,10 @@
 import { TonClient4 } from '@ton/ton';
-// import { getHttpV4Endpoint } from '@orbs-network/ton-access';
-import { TON_CLIENT_URL } from './config';
+import { getHttpV4Endpoint } from '@orbs-network/ton-access';
 
-// const endpointPromise = getHttpV4Endpoint();
+const endpointPromise = getHttpV4Endpoint();
 
 export const tonClient = (async () => {
-  const endpoint = TON_CLIENT_URL;
+  const endpoint = await endpointPromise;
 
   return new TonClient4({
     endpoint,
